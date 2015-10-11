af9f97e5fd02d2454e1efd7075621a43eca81282
ramhillel <rrr031289>
2015-10-11 21:37:57 +0300

    more checks 2

--- app/src/main/java/com/example/owner/wedding/MainActivity.java
+++ app/src/main/java/com/example/owner/wedding/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        boolean a  = false;
         return true;
     }
 
