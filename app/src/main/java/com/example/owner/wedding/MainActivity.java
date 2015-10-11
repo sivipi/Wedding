8b96485eedf226d1595f902e7c124e49b7db945c
ramhillel <rrr031289>
2015-10-11 21:36:20 +0300

    more checks

--- app/src/main/java/com/example/owner/wedding/MainActivity.java
+++ app/src/main/java/com/example/owner/wedding/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 //Comment for testing
+    //another check
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
